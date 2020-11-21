bc267783bbde95b2c4fbb1f39abac17cabb55a5f
IsaacOlufemi03 <72570021+IsaacOlufemi03@users.noreply.github.com>
2020-11-20 16:13:39 -0800

    Add files via upload

---
+++ php/process.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Document</title>
+</head>
+<body>
+    <?php
+
+    echo
+
+    for ($i=0 ; $i <  5; $i++ ) { 
+        doument.write('<form>The for loop has been executed</form>')
+        doument.write('<form>This is the "i++" run of the loop</form>')
+    }
+
+    ?>
+</body>
+</html>
